9c5ce27b8846fd43c255384cdfbf3d1d97f03da7
cuble <fouryu@sina.com>
2022-01-07 23:31:56 +0800

    implement to 3

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,9 @@
+public class FizzBuzz {
+    public String Say(int i) {
+        if(3==i){
+            return "Fizz";
+        }else {
+            return String.valueOf(i);
+        }
+    }
+}
--- src/test/java/FizzBuzzTests.java
+++ src/test/java/FizzBuzzTests.java
@@ -1,3 +1,31 @@
+import com.sun.tools.javac.util.Assert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTests {
+
+    @Test
+    @DisplayName("Say 1")
+    void BasePrintOne(){
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        assertEquals("1", fizzBuzz.Say(1));
+    }
+
+    @Test
+    @DisplayName("Say 2")
+    void BasePrintTwo(){
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        assertEquals("2", fizzBuzz.Say(2));
+    }
+
+    @Test
+    @DisplayName("Say 3")
+    void BasePrintThree(){
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        assertEquals("Fizz", fizzBuzz.Say(3));
+    }
+
 
 }
