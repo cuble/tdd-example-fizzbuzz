a76c2bd0116a4449555f936de5a6071e798d25ac
cuble <fouryu@sina.com>
2022-01-08 00:11:46 +0800

    testcases refactoring continue

--- src/test/java/FizzBuzzTests.java
+++ src/test/java/FizzBuzzTests.java
@@ -24,39 +24,33 @@ public class FizzBuzzTests {
         assertEquals(expectResult, fizzBuzz.Say(i), "Say " + i + " should equal " + expectResult);
     }
 
-    @Test
-    @DisplayName("Say 3")
-    void BasePrintThree(){
-        assertEquals("Fizz", fizzBuzz.Say(3));
+    @ParameterizedTest(name = "Say {0} = {1}")
+    @CsvSource({
+            "3,   Fizz",
+            "6,   Fizz",
+            "9,   Fizz"
+    })
+    void SayFizz(int i, String expectResult){
+        assertEquals(expectResult, fizzBuzz.Say(i), "Say " + i + " should equal " + expectResult);
     }
 
-    @Test
-    @DisplayName("Say 6")
-    void BasePrintSix(){
-        assertEquals("Fizz", fizzBuzz.Say(6));
+    @ParameterizedTest(name = "Say {0} = {1}")
+    @CsvSource({
+            "5,   Buzz",
+            "10,   Buzz",
+            "20,   Buzz"
+    })
+    void SayBuzz(int i, String expectResult){
+        assertEquals(expectResult, fizzBuzz.Say(i), "Say " + i + " should equal " + expectResult);
     }
 
-    @Test
-    @DisplayName("Say 5")
-    void BasePrintFive(){
-        assertEquals("Buzz", fizzBuzz.Say(5));
-    }
-
-    @Test
-    @DisplayName("Say 10")
-    void BasePrintTen(){
-        assertEquals("Buzz", fizzBuzz.Say(10));
-    }
-
-    @Test
-    @DisplayName("Say 15")
-    void BasePrintFifteen(){
-        assertEquals("FizzBuzz", fizzBuzz.Say(15));
-    }
-
-    @Test
-    @DisplayName("Say 45")
-    void BasePrintFortyFive(){
-        assertEquals("FizzBuzz", fizzBuzz.Say(45));
+    @ParameterizedTest(name = "Say {0} = {1}")
+    @CsvSource({
+            "15,   FizzBuzz",
+            "30,   FizzBuzz",
+            "45,   FizzBuzz"
+    })
+    void SayFizzBuzz(int i, String expectResult){
+        assertEquals(expectResult, fizzBuzz.Say(i), "Say " + i + " should equal " + expectResult);
     }
 }
