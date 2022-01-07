1a46f24297cbf8713bf7137e909f170805f664b8
cuble <fouryu@sina.com>
2022-01-07 23:51:44 +0800

    testcases refactoring

--- src/test/java/FizzBuzzTests.java
+++ src/test/java/FizzBuzzTests.java
@@ -1,36 +1,38 @@
-import com.sun.tools.javac.util.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTests {
+    FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    public void before() throws Exception {
+        fizzBuzz = new FizzBuzz();
+    }
 
     @Test
     @DisplayName("Say 1")
     void BasePrintOne(){
-        FizzBuzz fizzBuzz=new FizzBuzz();
         assertEquals("1", fizzBuzz.Say(1));
     }
 
     @Test
     @DisplayName("Say 2")
     void BasePrintTwo(){
-        FizzBuzz fizzBuzz=new FizzBuzz();
         assertEquals("2", fizzBuzz.Say(2));
     }
 
     @Test
     @DisplayName("Say 3")
     void BasePrintThree(){
-        FizzBuzz fizzBuzz=new FizzBuzz();
         assertEquals("Fizz", fizzBuzz.Say(3));
     }
 
     @Test
     @DisplayName("Say 6")
     void BasePrintSix(){
-        FizzBuzz fizzBuzz=new FizzBuzz();
         assertEquals("Fizz", fizzBuzz.Say(6));
     }
 
