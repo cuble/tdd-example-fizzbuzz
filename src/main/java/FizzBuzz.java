0a59005082032be56e2edd8ddaa582c72e3d074f
cuble <fouryu@sina.com>
2022-01-08 08:03:56 +0800

    code refactoring 3

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,6 +1,6 @@
 public class FizzBuzz {
     public String Say(int i) {
-        String result="";
+        String result;
         result = (i%3==0)?"Fizz":"";
         result = (i%5==0)?result+"Buzz":result;
         result = (result.equals(""))? String.valueOf(i):result;
